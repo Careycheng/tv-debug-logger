0a5ff2cd84f16be7af5d93af365b60750e6c38c8
Carey <43634828+Careycheng@users.noreply.github.com>
2026-01-09 16:26:14 +0800

    Initial commit: TV Debug Logger plugin

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import typescript from '@rollup/plugin-typescript';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import postcss from 'rollup-plugin-postcss';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+      sourcemap: true
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'es',
+      sourcemap: true
+    },
+    {
+      file: 'dist/index.umd.js',
+      format: 'umd',
+      name: 'TVDebugLogger',
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript({
+      tsconfig: './tsconfig.json'
+    }),
+    postcss({
+      extract: 'tv-debug-logger.css',
+      minimize: true
+    })
+  ]
+};
